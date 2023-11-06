03bbf65cb21de08bb00055d9bb2d73a02ce1902e
Oleg Shestakov <eithufter@gmail.com>
2023-11-06 16:08:15 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+int amount = 56_789;
+int cash = 20;
+int bonus = amount / cash;
+System.out.println("Итоговый бонус: " + bonus);
+
+    }
+}
